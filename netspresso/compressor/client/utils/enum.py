4c6aaa4f801f6339a22e1038e8f69b9550d7acf1
Only-bottle <bmlee@nota.ai>
2023-07-12 11:58:34 +0900

    modify literal dynamic -> static

--- netspresso/compressor/client/utils/enum.py
+++ netspresso/compressor/client/utils/enum.py
@@ -2,13 +2,7 @@ from enum import Enum
 from typing import Literal
 
 
-class ExtendedEnum(str, Enum):
-    @classmethod
-    def create_literal(cls):
-        return Literal[tuple(value.value for key, value in cls.__members__.items())]
-
-
-class Task(ExtendedEnum):
+class Task(str, Enum):
     IMAGE_CLASSIFICATION = "image_classification"
     OBJECT_DETECTION = "object_detection"
     IMAGE_SEGMENTATION = "image_segmentation"
@@ -17,21 +11,41 @@ class Task(ExtendedEnum):
     PANOPTIC_SEGMENTATION = "panoptic_segmentation"
     OTHER = "other"
 
+    @classmethod
+    def create_literal(cls):
+        return Literal[
+            "image_classification",
+            "object_detection",
+            "image_segmentation",
+            "semantic_segmentation",
+            "instance_segmentation",
+            "panoptic_segmentation",
+            "other",
+        ]
 
-class Framework(ExtendedEnum):
+
+class Framework(str, Enum):
     TENSORFLOW_KERAS = "tensorflow_keras"
     PYTORCH = "pytorch"
     ONNX = "onnx"
 
+    @classmethod
+    def create_literal(cls):
+        return Literal["tensorflow_keras", "pytorch", "onnx"]
 
-class Extension(ExtendedEnum):
+
+class Extension(str, Enum):
     H5 = "h5"
     ZIP = "zip"
     PT = "pt"
     ONNX = "onnx"
 
+    @classmethod
+    def create_literal(cls):
+        return Literal["h5", "zip", "pt", "onnx"]
 
-class CompressionMethod(ExtendedEnum):
+
+class CompressionMethod(str, Enum):
     PR_L2 = "PR_L2"
     PR_GM = "PR_GM"
     PR_NN = "PR_NN"
@@ -40,15 +54,27 @@ class CompressionMethod(ExtendedEnum):
     FD_CP = "FD_CP"
     FD_SVD = "FD_SVD"
 
+    @classmethod
+    def create_literal(cls):
+        return Literal["PR_L2", "PR_GM", "PR_NN", "PR_ID", "FD_TK", "FD_CP", "FD_SVD"]
 
-class RecommendationMethod(ExtendedEnum):
+
+class RecommendationMethod(str, Enum):
     SLAMP = "slamp"
     VBMF = "vbmf"
 
+    @classmethod
+    def create_literal(cls):
+        return Literal["slamp", "vbmf"]
 
-class OriginFrom(ExtendedEnum):
+
+class OriginFrom(str, Enum):
     CUSTOM = "custom"
     NPMS = "npms"
+
+    @classmethod
+    def create_literal(cls):
+        return Literal["custom", "npms"]
 
 
 task_literal = Task.create_literal()
