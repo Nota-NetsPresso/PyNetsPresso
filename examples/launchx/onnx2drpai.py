9bf64f2a632f38e4eaf72c8615807af82c31c91d
Sanggeon Park <gunnih@gmail.com>
2023-08-03 21:10:20 +0900

    updated examples

--- examples/launchx/onnx2drpai.py
+++ examples/launchx/onnx2drpai.py
@@ -21,25 +21,45 @@ if __name__ == '__main__':
     conversion_task: ConversionTask = converter.convert_model(model=model,
                                                               input_shape=model.input_shape,
                                                               target_framework=ModelFramework.DRPAI,
-                                                              target_device=available_devices[0])
+                                                              target_device=available_devices[0],
+                                                              wait_until_done=True)
+    ########################
+    # Asynchronous Procedure
+    # If you wish to request conversion and retrieve the results later, please refer to the following code.
+    ########################
+    # conversion_task: ConversionTask = converter.convert_model(model=model,
+    #                                                           input_shape=model.input_shape,
+    #                                                           target_framework=ModelFramework.DRPAI,
+    #                                                           target_device=available_devices[0],
+    #                                                           wait_until_done=False)
 
-    while conversion_task.status in [TaskStatus.IN_QUEUE, TaskStatus.IN_PROGRESS]:
-        conversion_task = converter.get_conversion_task(conversion_task)
-        logger.info(f"conversion task status : {conversion_task.status}")
-        time.sleep(1)
+    # while conversion_task.status in [TaskStatus.IN_QUEUE, TaskStatus.IN_PROGRESS]:
+    #     conversion_task = converter.get_conversion_task(conversion_task)
+    #     logger.info(f"conversion task status : {conversion_task.status}")
+    #     time.sleep(1)
 
-    conversion_task = converter.get_conversion_task(conversion_task)
+    # conversion_task = converter.get_conversion_task(conversion_task)
+
     logger.info(conversion_task)
     converter.download_converted_model(conversion_task, dst=CONVERTED_MODEL_PATH)
 
     
     benchmarker = ModelBenchmarker(session)
     benchmark_model: Model = benchmarker.upload_model(CONVERTED_MODEL_PATH)
-    benchmark_task: BenchmarkTask = benchmarker.benchmark_model(model=benchmark_model, target_device=target_device)
+    benchmark_task: BenchmarkTask = benchmarker.benchmark_model(model=benchmark_model,
+                                                                target_device=target_device,
+                                                                wait_until_done=True)
+    ########################
+    # Asynchronous Procedure
+    # If you wish to request conversion and retrieve the results later, please refer to the following code.
+    ########################
+    # benchmark_task: BenchmarkTask = benchmarker.benchmark_model(model=benchmark_model,
+    #                                                             target_device=target_device,
+    #                                                             wait_until_done=False)
 
-    while benchmark_task.status in [TaskStatus.IN_QUEUE, TaskStatus.IN_PROGRESS]:
-        benchmark_task = benchmarker.get_benchmark_task(benchmark_task=benchmark_task)
-        logger.info(f"benchmark task status : {benchmark_task.status}")
-        time.sleep(1)
+    # while benchmark_task.status in [TaskStatus.IN_QUEUE, TaskStatus.IN_PROGRESS]:
+    #     benchmark_task = benchmarker.get_benchmark_task(benchmark_task=benchmark_task)
+    #     logger.info(f"benchmark task status : {benchmark_task.status}")
+    #     time.sleep(1)
 
     logger.info(f"model inference latency: {benchmark_task.latency} ms")
