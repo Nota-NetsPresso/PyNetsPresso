4bea998b531f97b081be7c2c4630065c60cd8ea0
Sanggeon Park <gunnih@gmail.com>
2023-08-03 15:43:29 +0900

    added filter_devices_with_device_software_version

--- netspresso/launchx/utils/devices.py
+++ netspresso/launchx/utils/devices.py
@@ -1,6 +1,10 @@
-from netspresso.launchx.schemas import ModelFramework, DeviceName
+from netspresso.launchx.schemas import DeviceName
 from netspresso.launchx.schemas.model import TargetDevice
 
 def filter_devices_with_device_name(name: DeviceName, devices: list[TargetDevice]) -> list[TargetDevice]:
     filtered_devices = (device for device in devices if device.device_name is name)
+    return list(filtered_devices)
+
+def filter_devices_with_device_software_version(software_version: str, devices: list[TargetDevice]) -> list[TargetDevice]:
+    filtered_devices = (device for device in devices if device.software_version is software_version)
     return list(filtered_devices)
