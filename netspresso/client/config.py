fac46a28483eb62b0996384db6f885b78508528d
Sanggeon Park <gunnih@gmail.com>
2023-08-04 11:24:02 +0900

    lower case environment variables .

--- netspresso/client/config.py
+++ netspresso/client/config.py
@@ -6,32 +6,32 @@ from loguru import logger
 
 BASE_DIR = Path(__file__).resolve().parent
 config = configparser.ConfigParser()
-DEPLOYMENT_MODE = os.getenv("DEPLOYMENT_MODE", "PROD")
+DEPLOYMENT_MODE = os.getenv("DEPLOYMENT_MODE", "prod")
 logger.info(f"Read {DEPLOYMENT_MODE} config")
 config.read(f"{BASE_DIR}/configs/config.ini")
 
 class EnvironmentType(str, Enum):
-    PROD = "PROD"
-    LOCAL = "LOCAL"
+    PROD = "prod"
+    LOCAL = "dev"
 
     @classmethod
     def create_literal(cls):
         return Literal[
-            "PROD",
-            "LOCAL"
+            "prod",
+            "local"
         ]
 
 class EndPoint(str, Enum):
-    GENRAL = "GENERAL"
-    COMPRESSOR = "COMPRESSOR"
-    LAUNCHX = "LAUNCHX"
+    GENRAL = "general"
+    COMPRESSOR = "compressor"
+    LAUNCHX = "launchx"
 
     @classmethod
     def create_literal(cls):
         return Literal[
-            "GENERAL",
-            "COMPRESSOR",
-            "LAUNCHX"
+            "general",
+            "compressor",
+            "lauchx"
         ]
     
 class EndPointProperty(str, Enum):
@@ -56,7 +56,7 @@ class Config:
     URI_PREFIX: str = config[CONFIG_SESSION][EndPointProperty.URI_PREFIX]
 
     def __init__(self, endpoint: EndPoint = EndPoint.GENRAL):
-        self.ENVIRONMENT_TYPE = EnvironmentType(DEPLOYMENT_MODE.upper())
+        self.ENVIRONMENT_TYPE = EnvironmentType(DEPLOYMENT_MODE.lower())
         self.CONFIG_SESSION = f"{endpoint}.{self.ENVIRONMENT_TYPE}"
         self.HOST = config[self.CONFIG_SESSION][EndPointProperty.HOST]
         self.PORT = int(config[self.CONFIG_SESSION][EndPointProperty.PORT])
