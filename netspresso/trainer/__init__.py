81399be6b843b409c92ceaec68f178dc1c411df9
Only-bottle <bmlee@nota.ai>
2023-12-11 09:43:17 +0000

    Add augmentation in trainer/__init__.py

--- netspresso/trainer/__init__.py
+++ netspresso/trainer/__init__.py
@@ -1,5 +1,6 @@
 from netspresso.trainer.model_trainer import ModelTrainer
 from netspresso.trainer.enums import Backbone, Head, Task
+from netspresso_trainer.cfg.augmentation import ColorJitter, Pad, RandomCrop, RandomResizedCrop, RandomHorizontalFlip, RandomVerticalFlip, Resize, RandomMixup, RandomCutmix
 
 
 __all__ = ["ModelTrainer", "Backbone", "Head", "Task"]
