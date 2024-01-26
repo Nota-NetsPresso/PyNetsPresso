6e422eaab7ba51f202abed3f97bdc206b28eb0b3
Byeongman Lee <bmlee@nota.ai>
2024-01-26 17:14:37 +0900

    #118 Move trainer enum to common enum (#119)

--- netspresso/enums/model.py
+++ netspresso/enums/model.py
@@ -2,28 +2,6 @@ from enum import Enum
 from typing import Literal
 
 
-class Task(str, Enum):
-    IMAGE_CLASSIFICATION = "image_classification"
-    OBJECT_DETECTION = "object_detection"
-    IMAGE_SEGMENTATION = "image_segmentation"
-    SEMANTIC_SEGMENTATION = "semantic_segmentation"
-    INSTANCE_SEGMENTATION = "instance_segmentation"
-    PANOPTIC_SEGMENTATION = "panoptic_segmentation"
-    OTHER = "other"
-
-    @classmethod
-    def create_literal(cls):
-        return Literal[
-            "image_classification",
-            "object_detection",
-            "image_segmentation",
-            "semantic_segmentation",
-            "instance_segmentation",
-            "panoptic_segmentation",
-            "other",
-        ]
-
-
 class Framework(str, Enum):
     TENSORFLOW_KERAS = "tensorflow_keras"
     TENSORFLOW = "saved_model"
@@ -82,7 +60,6 @@ class DataType(str, Enum):
         return Literal["FP32", "FP16", "INT8", ""]
 
 
-task_literal = Task.create_literal()
 compressor_framework_literal = Framework.create_compressor_literal()
 launcher_framework_literal = Framework.create_launcher_literal()
 extension_literal = Extension.create_literal()
--- netspresso/trainer/__init__.py
+++ netspresso/trainer/__init__.py
@@ -1,4 +1,3 @@
-from netspresso.trainer.enums import Task
 from netspresso.trainer.trainer import Trainer
 
-__all__ = ["Trainer", "Task"]
+__all__ = ["Trainer"]
