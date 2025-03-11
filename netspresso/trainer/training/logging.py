02ea6bbe1221c138f1101b0060f816bf60ee91d6
Only-bottle <bmlee@nota.ai>
2025-03-11 12:31:51 +0000

    Add option to select best model saving criterion

--- netspresso/trainer/training/logging.py
+++ netspresso/trainer/training/logging.py
@@ -4,15 +4,11 @@ from typing import List, Optional, Union
 
 
 @dataclass
-class LoggingConfig:
-    project_id: Optional[str] = None
-    output_dir: Union[Path, str] = "./outputs"
-    tensorboard: bool = True
-    image: bool = True
-    stdout: bool = True
+class ModelSaveOptions:
     save_optimizer_state: bool = True
     save_best_only: bool = False
-    sample_input_size: List = field(default_factory=lambda: [512, 512])
+    best_model_criterion: str = "loss"  # metric or loss
+    sample_input_size: List[int] = field(default_factory=lambda: [512, 512])
     onnx_export_opset: int = 13  # Recommend in range [13, 17]
     validation_epoch: int = 10
     save_checkpoint_epoch: Optional[int] = None
@@ -20,3 +16,13 @@ class LoggingConfig:
     def __post_init__(self):
         if self.save_checkpoint_epoch is None:
             self.save_checkpoint_epoch = self.validation_epoch
+
+
+@dataclass
+class LoggingConfig:
+    project_id: Optional[str] = None
+    output_dir: Union[Path, str] = "./outputs"
+    tensorboard: bool = True
+    image: bool = True
+    stdout: bool = True
+    model_save_options: ModelSaveOptions = field(default_factory=ModelSaveOptions)
