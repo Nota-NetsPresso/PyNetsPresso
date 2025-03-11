109d28e8ee59194909abca08a98a8c78cf9fe035
Only-bottle <bmlee@nota.ai>
2025-03-11 14:27:08 +0000

    Update convert & benchmark celery task

--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -1,6 +1,4 @@
 from celery import Celery, chain
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import Session, scoped_session
 
 from app.services.user import user_service
 from netspresso.utils.db.session import SessionLocal
@@ -12,58 +10,6 @@ POLLING_INTERVAL = 30  # seconds
 connection_url = f"redis://:{REDIS_PASSWORD}@{REDIS_URL}" if REDIS_PASSWORD else f"redis://{REDIS_URL}"
 
 app = Celery("netspresso_converter", broker=f"{connection_url}/0", backend=f"{connection_url}/0")
-
-
-def create_scoped_session():
-    """Create a scoped session that will be removed after each request"""
-    return scoped_session(SessionLocal)
-
-
-def _process_with_session(func, *args, **kwargs):
-    """Execute function with a new session and ensure proper cleanup
-
-    Args:
-        func: Function to execute
-        args: Positional arguments for func
-        kwargs: Keyword arguments for func
-
-    Returns:
-        Result from func
-    """
-    session = create_scoped_session()
-    try:
-        result = func(session, *args, **kwargs)
-        session.commit()
-        return result
-    except SQLAlchemyError as e:
-        session.rollback()
-        raise e
-    finally:
-        session.remove()
-
-
-def _process_conversion(session: Session, api_key: str, **kwargs) -> str:
-    netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
-    converter = netspresso.converter_v2()
-    return converter.convert_model(**kwargs, wait_until_done=False)
-
-
-def _process_conversion_status(session: Session, api_key: str, task_id: str) -> bool:
-    netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
-    converter = netspresso.converter_v2()
-    return converter.update_conversion_task_status(task_id)
-
-
-def _process_benchmark(session: Session, api_key: str, **kwargs) -> str:
-    netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
-    benchmarker = netspresso.benchmarker_v2()
-    return benchmarker.benchmark_model(**kwargs, wait_until_done=False)
-
-
-def _process_benchmark_status(session: Session, api_key: str, task_id: str) -> bool:
-    netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
-    benchmarker = netspresso.benchmarker_v2()
-    return benchmarker.update_benchmark_task_status(task_id)
 
 
 @app.task
@@ -79,9 +25,14 @@ def convert_model_task(
     dataset_path: str = None,
     input_model_id: str = None,
 ):
-    task_id = _process_with_session(
-        _process_conversion,
-        api_key,
+    session = SessionLocal()
+    try:
+        netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
+    finally:
+        session.close()
+
+    converter = netspresso.converter_v2()
+    task_id = converter.convert_model(
         input_model_path=input_model_path,
         output_dir=output_dir,
         target_framework=target_framework,
@@ -91,6 +42,7 @@ def convert_model_task(
         input_layer=input_layer,
         dataset_path=dataset_path,
         input_model_id=input_model_id,
+        wait_until_done=False,
     )
 
     chain(poll_conversion_status.s(api_key, task_id).set(countdown=POLLING_INTERVAL))()
@@ -99,7 +51,14 @@ def convert_model_task(
 
 @app.task
 def poll_conversion_status(api_key: str, task_id: str):
-    status_updated = _process_with_session(_process_conversion_status, api_key, task_id)
+    session = SessionLocal()
+    try:
+        netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
+    finally:
+        session.close()
+
+    converter = netspresso.converter_v2()
+    status_updated = converter.update_conversion_task_status(task_id)
 
     if not status_updated:
         poll_conversion_status.apply_async(args=[api_key, task_id], countdown=POLLING_INTERVAL)
@@ -114,14 +73,20 @@ def benchmark_model_task(
     target_hardware_type: str = None,
     input_model_id: str = None,
 ):
-    task_id = _process_with_session(
-        _process_benchmark,
-        api_key,
+    session = SessionLocal()
+    try:
+        netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
+    finally:
+        session.close()
+
+    benchmarker = netspresso.benchmarker_v2()
+    task_id = benchmarker.benchmark_model(
         input_model_path=input_model_path,
         target_device_name=target_device_name,
         target_software_version=target_software_version,
         target_hardware_type=target_hardware_type,
         input_model_id=input_model_id,
+        wait_until_done=False,
     )
 
     chain(poll_benchmark_status.s(api_key, task_id).set(countdown=POLLING_INTERVAL))()
@@ -130,7 +95,14 @@ def benchmark_model_task(
 
 @app.task
 def poll_benchmark_status(api_key: str, task_id: str):
-    status_updated = _process_with_session(_process_benchmark_status, api_key, task_id)
+    session = SessionLocal()
+    try:
+        netspresso = user_service.build_netspresso_with_api_key(db=session, api_key=api_key)
+    finally:
+        session.close()
+
+    benchmarker = netspresso.benchmarker_v2()
+    status_updated = benchmarker.update_benchmark_task_status(task_id)
 
     if not status_updated:
         poll_benchmark_status.apply_async(args=[api_key, task_id], countdown=POLLING_INTERVAL)
