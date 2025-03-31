5f7378d76ec775f87690208a90841b6628a302f7
Only-bottle <bmlee@nota.ai>
2025-03-31 14:28:20 +0900

    Rename mosaic_off_epoch to mosaic_off_duration in MosaicDetection class for clarity

--- netspresso/trainer/augmentations/augmentation.py
+++ netspresso/trainer/augmentations/augmentation.py
@@ -64,7 +64,7 @@ class MosaicDetection(Transform):
     mixup_prob: float = 1.0
     mixup_scale: List = field(default_factory=lambda: [0.5, 1.5])
     fill: int = 114
-    mosaic_off_epoch: int = 10
+    mosaic_off_duration: int = 10
 
 
 @dataclass
