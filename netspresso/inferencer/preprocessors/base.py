78d8fda9d0e8b577ebfb2823d0de699c4f2f65df
Only-bottle <bmlee@nota.ai>
2025-05-17 13:51:25 +0000

    Remove assertion for resize_criteria in resize_img function to allow flexibility in input handling

--- netspresso/inferencer/preprocessors/base.py
+++ netspresso/inferencer/preprocessors/base.py
@@ -15,7 +15,7 @@ def resize_img(img, size, interpolation, max_size, resize_criteria):
     if isinstance(size, list):
         size = size[0]
     assert isinstance(size, int), "Only support int type ``size`` now."
-    assert resize_criteria == "long", "Only support ``long`` type ``resize_criteria`` now."
+    # assert resize_criteria == "long", "Only support ``long`` type ``resize_criteria`` now."
     h, w = img.shape[:2]
     long_side, short_side = max(h, w), min(h, w)
     resize_factor = size / long_side
