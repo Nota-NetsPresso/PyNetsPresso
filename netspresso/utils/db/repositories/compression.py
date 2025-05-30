b5b027b22bb5478d71db9384c5902c6e2dba760a
Only-bottle <bmlee@nota.ai>
2025-05-30 04:12:50 +0000

    Add compression task repository and exceptions for handling compression tasks

--- netspresso/utils/db/repositories/compression.py
+++ netspresso/utils/db/repositories/compression.py
@@ -1,9 +1,27 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from netspresso.exceptions.compression import CompressionTaskIsDeletedException, CompressionTaskNotFoundException
 from netspresso.utils.db.models.compression import CompressionTask
 from netspresso.utils.db.repositories.base import BaseRepository
 
 
 class CompressionTaskRepository(BaseRepository[CompressionTask]):
-    pass
+    def __is_available(self, task: Optional[CompressionTask]) -> CompressionTask:
+        if task is None:
+            raise CompressionTaskNotFoundException()
+
+        if task.is_deleted:
+            raise CompressionTaskIsDeletedException(task_id=task.task_id)
+
+        return task
+
+    def get_by_model_id(self, db: Session, model_id: str) -> CompressionTask:
+        conditions = [self.model.model_id == model_id]
+        task = self.find_first(db=db, conditions=conditions)
+
+        return self.__is_available(task=task)
 
 
 compression_task_repository = CompressionTaskRepository(CompressionTask)
