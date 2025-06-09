fe1e5933adbdb1e582a29d21b99d6f0642356b7e
Byeongman Lee <bmlee@nota.ai>
2025-06-10 00:40:50 +0900

    Reorder optimizers list in get_supported_optimizers function (#527)

--- netspresso/trainer/optimizers/optimizers.py
+++ netspresso/trainer/optimizers/optimizers.py
@@ -79,5 +79,5 @@ class SGD(BaseOptimizer):
 
 def get_supported_optimizers() -> List[Dict[str, Any]]:
     """Return a list of supported optimizers with their parameters and default values."""
-    optimizers = [Adadelta(), Adagrad(), Adam(), Adamax(), AdamW(), RMSprop(), SGD()]
+    optimizers = [AdamW(), Adadelta(), Adagrad(), Adam(), Adamax(), RMSprop(), SGD()]
     return [{"name": optimizer.name, "parameters": optimizer.to_parameters()} for optimizer in optimizers]
