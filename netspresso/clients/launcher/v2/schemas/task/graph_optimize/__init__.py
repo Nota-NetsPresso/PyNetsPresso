e45cef06ee6b7cda0afc5125848f7119c985007d
Only-bottle <bmlee@nota.ai>
2025-06-11 01:50:51 +0900

    Add graph optimization request and response schemas

---
+++ netspresso/clients/launcher/v2/schemas/task/graph_optimize/__init__.py
@@ -0,0 +1,7 @@
+from .request_body import RequestCreateGraphOptimizeTask
+from .response_body import ResponseGraphOptimizeTaskItem
+
+__all__ = [
+    "RequestCreateGraphOptimizeTask",
+    "ResponseGraphOptimizeTaskItem",
+]
---
+++ netspresso/clients/launcher/v2/schemas/task/graph_optimize/response_body.py
@@ -0,0 +1,38 @@
+import dataclasses
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List, Optional
+
+from netspresso.clients.launcher.v2.schemas import InputLayer, ResponseItem
+from netspresso.enums import TaskStatusForDisplay
+from netspresso.enums.graph_optimize import GraphOptimizePatternHandler
+
+
+@dataclass
+class GraphOptimizeTask:
+    graph_optimize_task_id: str
+    status: TaskStatusForDisplay = ""
+    input_model_id: str
+    user_id: str
+    output_model_id: str
+    model_file_name: str
+    error_log: Optional[dict] = None
+    created_at: datetime = field(default_factory=datetime.now)
+    pattern_handlers: List[GraphOptimizePatternHandler]
+    input_layers: List[InputLayer] = field(default_factory=[])
+
+    def __init__(self, **kwargs):
+        names = {f.name for f in dataclasses.fields(self)}
+        for k, v in kwargs.items():
+            if k in names:
+                setattr(self, k, v)
+
+        self.input_layers = [InputLayer(**input_layers) for input_layers in self.input_layers]
+
+
+@dataclass
+class ResponseGraphOptimizeTaskItem(ResponseItem):
+    data: Optional[GraphOptimizeTask] = field(default_factory=dict)
+
+    def __post_init__(self):
+        self.data = GraphOptimizeTask(**self.data)
