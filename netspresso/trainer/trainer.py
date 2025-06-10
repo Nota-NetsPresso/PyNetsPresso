643bdf1f26b86e8e62286eca9a337441f2033c07
Only-bottle <bmlee@nota.ai>
2025-06-10 19:49:51 +0000

    Add method to save evaluation dataset in Trainer class

--- netspresso/trainer/trainer.py
+++ netspresso/trainer/trainer.py
@@ -59,6 +59,7 @@ from netspresso.utils.db.models.training import (
     Performance,
     TrainingTask,
 )
+from netspresso.utils.db.repositories.evaluation import evaluation_dataset_repository
 from netspresso.utils.db.repositories.model import model_repository
 from netspresso.utils.db.repositories.training import training_task_repository
 from netspresso.utils.db.session import get_db_session
@@ -338,6 +339,12 @@ class Trainer(NetsPressoBase):
             storage_info={"dataset_id": Path(root_path).name}
         )
 
+    def _save_evaluation_dataset(self, evaluation_dataset):
+        with get_db_session() as db:
+            evaluation_dataset = evaluation_dataset_repository.save(db=db, evaluation_dataset=evaluation_dataset)
+
+            return evaluation_dataset
+
     def set_test_dataset(self, dataset_root_path: str, dataset_name: Optional[str] = None):
         if dataset_name is None:
             dataset_name = Path(dataset_root_path).name
@@ -374,6 +381,7 @@ class Trainer(NetsPressoBase):
             storage_location=StorageLocation.STORAGE if self.is_dataforge else StorageLocation.LOCAL,
             storage_info={"dataset_id": Path(root_path).name}
         )
+        self.test_dataset = self._save_evaluation_dataset(self.test_dataset)
 
     def set_test_dataset_no_create(self, dataset_root_path: str, dataset_name: Optional[str] = None):
         if dataset_name is None:
