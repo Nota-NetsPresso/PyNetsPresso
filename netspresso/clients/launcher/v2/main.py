63f5fbb293a7a288caff4860b1d8ee051901a91c
Only-bottle <bmlee@nota.ai>
2025-06-11 02:50:32 +0900

    Add graph optimizer API integration in LauncherAPIClient

--- netspresso/clients/launcher/v2/main.py
+++ netspresso/clients/launcher/v2/main.py
@@ -1,6 +1,7 @@
 from netspresso.clients.config import Config, ServiceModule, ServiceName
 from netspresso.clients.launcher.v2.benchmarker import Benchmarker
 from netspresso.clients.launcher.v2.converter import Converter
+from netspresso.clients.launcher.v2.graph_optimizer import GraphOptimizer
 from netspresso.clients.launcher.v2.quantizer import Quantizer
 
 
@@ -14,6 +15,7 @@ class LauncherAPIClient:
         self.converter = self._create_convert_api()
         self.benchmarker = self._create_benchmark_api()
         self.quantizer = self._create_quantize_api()
+        self.graph_optimizer = self._create_graph_optimizer_api()
 
     def _create_convert_api(self):
         return Converter(self.url)
@@ -23,6 +25,9 @@ class LauncherAPIClient:
 
     def _create_quantize_api(self):
         return Quantizer(self.url)
+
+    def _create_graph_optimizer_api(self):
+        return GraphOptimizer(self.url)
 
     def is_cloud(self) -> bool:
         return self.config.is_cloud()
