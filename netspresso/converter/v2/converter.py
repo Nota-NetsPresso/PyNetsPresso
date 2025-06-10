ec60a9185d522c538b0f4b2e8bc05f89ad5c119d
Byeongman Lee <bmlee@nota.ai>
2025-06-10 14:00:08 +0900

    Fix path construction for calibration dataset in compressed model handling (#535)

--- netspresso/converter/v2/converter.py
+++ netspresso/converter/v2/converter.py
@@ -290,7 +290,7 @@ class ConverterV2(NetsPressoBase):
                 if input_model.type == SubFolder.COMPRESSED_MODELS:
                     compression_task = self.get_compression_task(model_id=input_model.model_id)
                     input_model = self.get_input_model(input_model_id=compression_task.input_model_id, user_id=self.user_info.user_id)
-                    remote_calibration_dataset_path = Path(input_model.object_path).parent / "calibration_dataset.npy"
+                    remote_calibration_dataset_path = Path(input_model.object_path) / "calibration_dataset.npy"
                 else:
                     remote_calibration_dataset_path = Path(input_model.object_path) / "calibration_dataset.npy"
                 local_calibration_dataset_path = download_dir / "calibration_dataset.npy"
