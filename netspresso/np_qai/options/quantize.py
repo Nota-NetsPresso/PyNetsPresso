52db2417cefc0804638f3e1e25de96abba7e8984
Only-bottle <bmlee@nota.ai>
2025-07-29 16:05:01 +0900

    Update documentation link format for QuantizeOptions in QAI Hub API

--- netspresso/np_qai/options/quantize.py
+++ netspresso/np_qai/options/quantize.py
@@ -24,7 +24,7 @@ class QuantizeOptions:
     Quantize options for the model.
 
     Note:
-        For details, see [QuantizeOptions in QAI Hub API](https://app.aihub.qualcomm.com/docs/hub/api.html#quantize-options).
+        For details, see `QuantizeOptions in QAI Hub API <https://app.aihub.qualcomm.com/docs/hub/api.html#quantize-options>`_.
     """
 
     range_scheme: Optional[RangeScheme] = RangeScheme.AUTO
